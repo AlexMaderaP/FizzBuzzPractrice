9d17f8dafcddcd070b287ce9afa0b30e432e7e0b
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-26 12:47:47 -0500

    Agregando modulo ExplorerService

--- app.js
+++ app.js
@@ -2,15 +2,16 @@
 const Reader = require("./lib/utils/Reader");
 const explorers = Reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
 
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+// Aplicación del ExplorerService sobre la lista de explorers
+const ExplorerService = require("./lib/services/ExplorerService")
 
-// Part4: Get the explorer's usernames in Node
-const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-//console.log(usernamesInNode)
+ExplorerService.filterByMission(explorers, "node")
+ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+ExplorerService.getExplorersUsernamesByMission(explorers, "node")
 
+
+
+/*
 // DEAD CODE: Part 5,6,7, please remove this and go to Part 8!
 
 // Part 5: Get a new list of explorers in node, if the score numbers is divisible by 3, I need a new propery called trick, and the value assigned is FIZZ, if not the value should be the score itself.
@@ -59,3 +60,4 @@ const assignFizzBuzzTrick = function(explorer){
 const explorersInNodeAndFizzBuzzTrick = explorersInNode.map((explorer) => assignFizzBuzzTrick(explorer));
 
 // Part 8: Get a list of the explorers in node, if the score is divisible by 5 and 3, set the property trick and the value FIZZBUZZ, if is just divisible by 5 set the property trcik and the value BUZZ, if is just divisible by 3 set the property trick and the value FIZZ, otherwise set the property trick and the score value. TODO
+*/
