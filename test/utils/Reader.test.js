015713f9129f949b2b45cc1fbed217e2b9a71346
AlexMaderaP <alejandro.maderap@gmail.com>
2022-04-26 14:27:06 -0500

    Agregando linter al proyecto

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,12 +1,9 @@
-const Reader = require('./../../lib/utils/Reader');
+const Reader = require("../../lib/utils/Reader");
 
 describe("Unit Tests for Reader Class", () => {
-    test('Read a JSON file', () => {
-
+    test("Read a JSON file", () => {
         const explorers = Reader.readJsonFile("explorers.json");
 
         expect(explorers).not.toBe(undefined);
-
     });
-  })
-  
+});
